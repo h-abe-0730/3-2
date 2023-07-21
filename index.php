56b8d354974333a963f57eda38266052ff0fc0b4
Hiromi-Abe <h_abe_0730@yahoo.co.jp>
2023-07-21 09:32:34 +0900

    PHPエンジニアコース 3章 練習問題1
    前回提出後、ご指摘いただいた内容をもとに修正しました。
    ご確認お願いいたします。

--- index.php
+++ index.php
@@ -1,18 +1,30 @@
 <?php
 
-define("TAX" , "0.1");
+define("TAX", 0.1);
 
-$products = ["鉛筆" =>100 , "消しゴム" =>150 , "物差し" =>500 ];
+$products = ["鉛筆" => 100, "消しゴム" => 150, "物差し" => 500];
 
-function price($products){
-    
-    foreach($products as $key => $value){
-        $price =$value * (1+TAX);
-        echo $key."の税込み価格は".$price."円です";
-        echo "<br>";
-    }
+foreach  ($products as $key => $price) {
+    echo $key. "の税込み価格は" .($price + ($price * TAX)). "円です";
+    echo '<br>';
 }
 
-price($products);
+
+// 2023/07/11 21:32提出
+
+// define("TAX" , "0.1");
+
+// $products = ["鉛筆" =>100 , "消しゴム" =>150 , "物差し" =>500 ];
+
+// function price($products){
+    
+//     foreach($products as $key => $value){
+//         $price =$value * (1+TAX);
+//         echo $key."の税込み価格は".$price."円です";
+//         echo "<br>";
+//     }
+// }
+
+// price($products);
 
 ?>
